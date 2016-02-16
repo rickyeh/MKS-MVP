56b6e1308f645f2844fcdd99ab35f7ca5cd0575b
RickYeh <RickYeh@users.noreply.github.com>
2016-02-15 17:00:37 -0800

    Added flickr API requests from server

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
+var keys = require('./keys.js')
 var app = express();
+
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+      api_key: keys.flickr_api,
+      secret: keys.secret_api
+    };
+
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -16,14 +24,26 @@ app.use(function(req, res, next) {
     next();
 });
 
-// app.get('/', function(req, res) {
-//   console.log('GET to home');
-// });
-
 app.post('/', function(req, res) {
   console.log('Received POST from client');
   var searchString = Object.keys(req.body)[0];
   console.log('Request Body : ', searchString);
+
+  var searchResults = [];
+
+  // Make a flickr api call 
+  Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    console.log('Inside tokenOnly');
+    flickr.photos.search({ text: searchString }, function(err, result) {
+      if (err) {
+        throw new Error(err);
+      }
+
+      console.log(result.photos.photo[0]);
+    });
+  // we can now use "flickr" as our API object,
+  // but we can only call public methods and access public data
+});
 
   
   res.end(JSON.stringify({'Post':'Received'}), 201);
